2a426654d80deb87228edd03b8f5cb27790154d5
gaurgarvit <138962874+gaurgarvit@users.noreply.github.com>
2025-06-09 11:32:57 +0530

    Your commit message

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,10 @@ import java.util.*;
 
 public class HelloWorld{
     public static void main(String[] args){
-        System.out.println("Hello World!!");
+        for(int i=0;i<101;i++){
+			if(i%2==0){
+				System.out.println(i);
+			}
+		}
     }
 }
