758d2b0028c7065989abb0a291352113129fabd2
gaurgarvit <138962874+gaurgarvit@users.noreply.github.com>
2025-06-09 11:45:09 +0530

    Prime

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,9 +3,19 @@ import java.util.*;
 public class HelloWorld{
     public static void main(String[] args){
         for(int i=0;i<101;i++){
-			if(i%2==0){
+			if(isPrime(i)){
 				System.out.println(i);
 			}
 		}
     }
+	
+	public static boolean isPrime(int n){
+		for(int i=2;i<n;i++){
+			if(n%i==0){
+				return false;
+			}
+		}
+		
+		return true;
+	}
 }
