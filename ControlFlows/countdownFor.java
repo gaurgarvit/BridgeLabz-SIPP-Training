7295bb9b5856d48e099d6aa1747f64f7b8838ff5
gaurgarvit <138962874+gaurgarvit@users.noreply.github.com>
2025-06-13 09:50:18 +0530

    Count down the numbers

---
+++ ControlFlows/countdownFor.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class countdownFor {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int counter = s.nextInt();
+
+        for (int i=counter;i>=1;i--){
+            System.out.println(i);
+        }
+        System.out.println("Lift off!");
+    }
+}
