2b61bcd3e90009b5b7ddc0f01ba9f66463fabd4c
gaurgarvit <138962874+gaurgarvit@users.noreply.github.com>
2025-06-13 09:55:13 +0530

    count down the numbers using while

---
+++ ControlFlows/countdownWhile.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class countdownWhile {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        int counter= s.nextInt();
+
+        while(counter>=1){
+            System.out.println(counter);
+            counter--;
+        }
+        System.out.println("Lift off!");
+    }
+}
