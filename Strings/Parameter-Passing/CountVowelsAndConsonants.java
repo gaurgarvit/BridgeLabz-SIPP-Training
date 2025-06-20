0dac649ae8bc6cfa90d3f3e96d3ec04f01884e91
gaurgarvit <138962874+gaurgarvit@users.noreply.github.com>
2025-06-20 22:37:41 +0530

    {Garvit} Add . Count Vowels and Consonants

--- Strings/Parameter-Passing/CountVowelsAndConsonants.java
+++ Strings/Parameter-Passing/CountVowelsAndConsonants.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class CountVowelsAndConsonants {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner s= new Scanner(System.in);
         System.out.println("Enter a string:");
 
         String input = s.nextLine();
