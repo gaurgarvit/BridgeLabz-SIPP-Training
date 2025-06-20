f13a8cc26cea1a1601f0e0e3b62effdbcea4aa4e
gaurgarvit <138962874+gaurgarvit@users.noreply.github.com>
2025-06-20 22:38:11 +0530

    {Garvit} Add . Find Longest Word in Sentence

--- Strings/Parameter-Passing/FindLongestWordInSentence.java
+++ Strings/Parameter-Passing/FindLongestWordInSentence.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class FindLongestWordInSentence {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner s= new Scanner(System.in);
         System.out.println("Enter a sentence:");
 
         String sentence = s.nextLine();
